355485899bd476c6ab0640bd9679a2543aaa3852
Lena k <ktyecz09@mail.ru>
2023-04-08 16:08:05 +0300

    add initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,36 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("every{it.balance>=0}", is(true))
+                .body("", hasSize(3))
+                .body("[1].currency", equalTo("USD"))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
+
+    }
+}
